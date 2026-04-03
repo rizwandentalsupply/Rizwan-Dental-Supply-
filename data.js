675218f5637f01f85d399cb44b2b140b74f35276
rizwandentalsupply <wahabrizwan652@gmail.com>
2026-04-03 19:09:46 +0500

    Add productDetails object with product information

---
+++ data.js
@@ -0,0 +1,7 @@
+const productDetails = {
+    "p1": {
+        "name": "Plastic Instrument",
+        "price": "150",
+        "desc": "dental plastic instrument is used to place and shape filling material in a tooth cavity."
+    }
+};
